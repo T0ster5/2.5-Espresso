8d46f1e1ddd22f6c75791995af666e93bcbf2043
T0ster5 <tester5@gaps.edu.ru>
2022-09-13 15:23:33 +0300

    add test negative

--- simpleAppForEspresso/app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/NewTest.java
+++ simpleAppForEspresso/app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/NewTest.java
@@ -1,11 +1,13 @@
 package ru.kkuzmichev.simpleappforespresso;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.contrib.DrawerActions;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
@@ -31,6 +33,25 @@ public class NewTest {
                 )
         );
     }
+
+
+    @Test
+    public void testCheckName() {
+
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+
+        onView(withId(R.id.nav_slideshow)).perform(click());
+
+        ViewInteraction mainText = onView(
+                withId(R.id.text_slideshow)
+        );
+        mainText.check(
+                matches(
+                        withText("This is home fragment")
+                )
+        );
+    }
+
 
 
 }
